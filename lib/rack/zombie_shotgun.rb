894eda00605b25a65672c7a6ccd0e8b71e0a89d5
Ken Collins <ken@metaskills.net>
2009-10-10 16:40:26 -0400

    Initial checkin.

---
+++ lib/rack/zombie_shotgun.rb
@@ -0,0 +1,44 @@
+module Rack
+  class ZombieShotgun
+    
+    ZOMBIE_AGENTS       = ['FrontPage',
+                           'Microsoft Office Protocol Discovery',
+                           'Microsoft Data Access Internet Publishing Provider Protocol Discovery',
+                           'Microsoft Data Access Internet Publishing Provider Cache Manager']
+    ZOMBIE_ATTACK_DIRS  = ['_vti_bin','MSOffice','verify-VCNstrict','notified-VCNstrict']
+    
+    def initialize(app, options={})
+      @app = app
+    end
+    
+    def call(env)
+      zombie_attack? ? head_not_found : @app.call(env)
+    end
+    
+    
+    private
+    
+    def head_not_found
+      [404, {"Content-Length" => "0"}, []]
+    end
+
+    def zombie_attack?
+      zombie_attack_on_directory? || zombie_agent_attack?
+    end
+
+    def zombie_attack_on_directory?
+      request_path = request.path.from(1)
+      request_dir = request_path.index('/').nil? ? request_path : request_path.to(request_path.index('/')-1)    
+      ZOMBIE_ATTACK_DIRS.include?(request_dir)
+    end
+
+    def zombie_agent_attack?
+      ua = request.env['HTTP_USER_AGENT']
+      !ua.blank? && ZOMBIE_AGENTS.any? { |za| ua =~ /#{za}/ }
+    end
+
+    
+  end
+end
+
+
