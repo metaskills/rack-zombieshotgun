3dc62f8fd754ea754039b1a1715606a9fa1cf5f2
Ken Collins <ken@metaskills.net>
2009-10-10 21:21:24 -0400

    Basic implementation for rack user agent and path inspection.

--- lib/rack-zombieshotgun.rb
+++ lib/rack-zombieshotgun.rb
@@ -1,18 +1,25 @@
 module Rack
   class ZombieShotgun
     
-    ZOMBIE_AGENTS       = ['FrontPage',
-                           'Microsoft Office Protocol Discovery',
-                           'Microsoft Data Access Internet Publishing Provider Protocol Discovery',
-                           'Microsoft Data Access Internet Publishing Provider Cache Manager'].freeze
+    ZOMBIE_AGENTS = [
+      'FrontPage',
+      'Microsoft Office Protocol Discovery',
+      'Microsoft Data Access Internet Publishing Provider Protocol Discovery',
+      'Microsoft Data Access Internet Publishing Provider Cache Manager'
+    ].freeze
 
-    ZOMBIE_ATTACK_DIRS  = ['_vti_bin','MSOffice','verify-VCNstrict','notified-VCNstrict'].freeze
+    ZOMBIE_DIRS = ['_vti_bin','MSOffice','verify-VCNstrict','notified-VCNstrict'].freeze
+    
+    attr_reader :options, :request, :agent
     
     def initialize(app, options={})
       @app = app
+      @options = options
     end
     
     def call(env)
+      @agent = env['HTTP_USER_AGENT']
+      @request = Rack::Request.new(env)
       zombie_attack? ? head_not_found : @app.call(env)
     end
     
@@ -24,18 +31,18 @@ module Rack
     end
 
     def zombie_attack?
-      zombie_attack_on_directory? || zombie_agent_attack?
+      zombie_dir_attack? || zombie_agent_attack?
     end
 
-    def zombie_attack_on_directory?
-      # request_path = request.path.from(1)
-      # request_dir = request_path.index('/').nil? ? request_path : request_path.to(request_path.index('/')-1)    
-      # ZOMBIE_ATTACK_DIRS.include?(request_dir)
+    def zombie_dir_attack?
+      path = request.path_info
+      ZOMBIE_DIRS.any? { |dir| path.include?("/#{dir}/") }
     end
 
     def zombie_agent_attack?
-      # ua = request.env['HTTP_USER_AGENT']
-      # !ua.blank? && ZOMBIE_AGENTS.any? { |za| ua =~ /#{za}/ }
+      agent && ZOMBIE_AGENTS.any? do |za|
+        agent =~ /#{za}/
+      end
     end
 
     
--- test/rack-zombieshotgun_test.rb
+++ test/rack-zombieshotgun_test.rb
@@ -4,18 +4,25 @@ class ZombieShotgunTest < Test::Unit::TestCase
   
   include Rack::Test::Methods
   
+  def setup
+    app_with Proc.new { |env| [200, {"Content-Type" => "text/html"}, "good"] }
+  end
+  
   context 'Basic setup' do
-
-    setup do
-      app_with Proc.new { |env| [200, {"Content-Type" => "text/html"}, "good"] }
-    end
 
     should 'get root' do
       get '/'
       assert last_response.ok?
     end
+    
+    should 'issue not found head when attacked' do
+      get '/_vti_bin/foo/bar'
+      assert last_response.not_found?
+      assert_equal '', last_response.body
+    end
 
   end
+  
   
 
   protected
